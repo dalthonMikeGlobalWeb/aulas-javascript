84df8ab8b1bd1cf63649b7d2a35244c1cf5497ab
dalthonMikeGlobalWeb <dalthon.silva@globalweb.com.br>
2021-10-25 16:55:30 -0300

    [Inclusão de atividades] - Atividades realizadas no curso básico de javascript

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// var nome = "Dalthon Mike";
+// var n1 = 19;
+// var n2= 10;
+// var frase = "Japão é o melhor time do mundo"
+// alert(nome + " tem " + n1 + " anos")
+// alert(n1 + n2);
+// console.log(nome);
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase());
+// var lista = ["Maça", "Pera"];
+// lista.push("Morango");
+// console.log(lista);
+// console.log(lista.join(" - "));
+// var idade = prompt("Qual sua idade?")
+//
+// if (idade >= 18) {
+//     alert("Maior de idade")
+// } else {
+//     alert("Menor de idade")
+// }
+// var contador;
+//
+// for (contador = 0; contador <= 5; contador++) {
+//     console.log(contador);
+// }
+// var d = new Date();
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMinutes());
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+// function setReplace(frase, nome, novoNome) {
+//    return frase.replace(nome, novoNome)
+// }
+// alert(soma(5, 10))
+// alert(setReplace("Vai japão", "japão", "Brasil"));
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+// function validaIdade(idade) {
+//     var validade;
+//     if (idade >= 18) {
+//        return validade = true;
+//     } else {
+//        return validade = false;
+//     }
+// }
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade));
+//function botao() {
+    // alert("Obrigado por clicar");
+    // document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+
+//}
+
+function redirecionar() {
+    window.open("www.google.com.br")
+}
